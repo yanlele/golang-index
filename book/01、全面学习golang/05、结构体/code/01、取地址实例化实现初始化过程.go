075d53e2914ac10d9f2b90f1aa018bc9b9c71356
yanlele <331393627@qq.com>
2020-02-29 13:39:32 +0800

    feat: 结构体定义

---
+++ book/01、全面学习golang/05、结构体/code/01、取地址实例化实现初始化过程.go
@@ -0,0 +1,26 @@
+package main
+
+type Command struct {
+	Name    string
+	Var     *int
+	Comment string
+}
+
+func newCommand(name string, varref *int, comment string) *Command {
+	return &Command{
+		Name:    name,
+		Var:     varref,
+		Comment: comment,
+	}
+}
+
+func main() {
+	version := 12
+	cmd := newCommand("version", &version, "show  version")
+
+	cmd2 := &Command{
+		Name:    "version",
+		Var:     &version,
+		Comment: "show version",
+	}
+}
