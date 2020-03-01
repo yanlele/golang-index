3b024987e83897e3e520bbdcd7f1a859cbe26c30
yanlele <331393627@qq.com>
2020-03-01 23:15:35 +0800

    feat: 互斥锁（sync.Mutex）和读写互斥锁（sync.RWMutex）

--- book/01、全面学习golang/08、并发/code/15、锁的概念/15-01、锁的经典实用模式/main/main.go
+++ book/01、全面学习golang/08、并发/code/15、锁的概念/15-01、锁的经典实用模式/main/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	count int
+	countGuard sync.Mutex
+)
+
+func GetCount() int {
+	// 锁定
+	countGuard.Lock()
+
+	defer countGuard.Unlock()
+	return count
+}
+
+func SetCount(c int) {
+	countGuard.Lock()
+	count = c
+	countGuard.Unlock()
+}
+
+func main() {
+	// 可以进行安全的并发设置
+	SetCount(1)
+	SetCount(3)
+
+	fmt.Println(GetCount())
+}
--- book/01、全面学习golang/08、并发/code/15、锁的概念/15-02、读写锁的示例/main/main.go
+++ book/01、全面学习golang/08、并发/code/15、锁的概念/15-02、读写锁的示例/main/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	count int
+	countGuard sync.RWMutex
+)
+
+func getCount() int {
+	countGuard.RLock()
+	defer countGuard.RUnlock()
+	return count
+}
+
+func setCount(a int) {
+	countGuard.Lock()
+	defer countGuard.Unlock()
+	count = a
+}
+
+func main() {
+	setCount(2)
+	fmt.Println(getCount())
+}
