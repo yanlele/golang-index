c51ca92c72aadb64f660744e2b59c7458cc35e98
yanlele <331393627@qq.com>
2020-11-16 09:24:10 +0800

    feat: 删除不必要的测试

--- packages/gin_series/04_form/test/user_test.go
+++ packages/gin_series/04_form/test/user_test.go
@@ -3,45 +3,11 @@ package test
 import (
 	"bytes"
 	"github.com/stretchr/testify/assert"
-	"go-index/packages/gin_series/02_router/initRouter"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
-	"strconv"
 	"testing"
 )
-
-
-func TestUserSave(t *testing.T) {
-	username := "lisi"
-	router := initRouter.SetupRouter()
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodGet, "/user/"+username, nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, "用户"+username+"已经保存", w.Body.String())
-}
-
-func TestUserSaveQuery(t *testing.T) {
-	username := "lisi"
-	age := 18
-	router := initRouter.SetupRouter()
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodGet, "/user/?name="+username+"&age="+strconv.Itoa(age), nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, "用户:"+username+",年龄:"+strconv.Itoa(age)+"已经保存", w.Body.String())
-}
-
-func TestUserSaveWithNotAge(t *testing.T) {
-	username := "lisi"
-	router := initRouter.SetupRouter()
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodGet, "/user/?name="+username, nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, "用户:"+username+",年龄:20已经保存", w.Body.String())
-}
 
 func TestUserPostForm(t *testing.T) {
 	value := url.Values{}
