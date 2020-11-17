1f740de3e362f108ec7e43f13ded7ae4ad76f88f
yanlele <331393627@qq.com>
2020-11-17 23:01:56 +0800

    feat: 添加测试

--- packages/gin_series/05_db/test/user_test.go
+++ packages/gin_series/05_db/test/user_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
 )
 
@@ -31,5 +32,18 @@ func TestUserPostFormEmailErrorAndPasswordError(t *testing.T) {
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; param=value")
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestUserLogin(t *testing.T) {
+	email := "youngxhui@163.com"
+	value := url.Values{}
+	value.Add("email", email)
+	value.Add("password", "1234")
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/user/login", bytes.NewBufferString(value.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; param=value")
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, strings.Contains(w.Body.String(), email), true)
 }
 
