2e1e84c18b8c8b299a159f0869366dc0a0ef9a0a
yanlele <331393627@qq.com>
2020-11-20 22:42:11 +0800

    feat: 修改模板

--- packages/gin_series/06_upload_file/initRouter/initRouter.go
+++ packages/gin_series/06_upload_file/initRouter/initRouter.go
@@ -10,7 +10,7 @@ func SetupRouter() *gin.Engine {
 	if mode := gin.Mode(); mode == gin.TestMode {
 		router.LoadHTMLGlob("../templates/*")
 	} else {
-		router.LoadHTMLGlob("packages/gin_series/05_db/templates/*")
+		router.LoadHTMLGlob("packages/gin_series/06_upload_file/templates/*")
 	}
 	router.StaticFile("/favicon.ico", "packages/gin_series/03_templates/favicon.ico")
 	router.Static("/statics", "packages/gin_series/03_templates/statics")
