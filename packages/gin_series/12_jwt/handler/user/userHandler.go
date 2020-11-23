616a9ef3c5e71b75dab602bba44186ccb01be552
yanlele <331393627@qq.com>
2020-11-23 09:00:01 +0800

    feat: CreateJwt

--- packages/gin_series/12_jwt/handler/user/userHandler.go
+++ packages/gin_series/12_jwt/handler/user/userHandler.go
@@ -1,8 +1,65 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"go-index/packages/gin_series/12_jwt/config"
+	"go-index/packages/gin_series/12_jwt/model"
+	"net/http"
+	"time"
+)
 
 func CreateJwt(context *gin.Context) {
 	// 获取用户信息
-	user:= &model.user
+	user := &model.User{}
+	result := &model.Result{
+		Code:    http.StatusOK,
+		Message: "登录成功",
+		Data:    nil,
+	}
+	e := context.BindJSON(&user)
+	if e != nil {
+		result.Message = "数据绑定失败"
+		result.Code = http.StatusUnauthorized
+		context.JSON(http.StatusUnauthorized, gin.H{
+			"result": result,
+		})
+	}
+
+	u := user.QueryByUsername()
+	if u.Password == user.Password {
+		expiresTime := time.Now().Unix() + int64(config.OndDayOfHours)
+		claims := jwt.StandardClaims{
+			Audience:  user.Username,     // 受众
+			ExpiresAt: expiresTime,       // 失效时间
+			Id:        string(user.ID),   // 编号
+			IssuedAt:  time.Now().Unix(), // 签发时间
+			Issuer:    "gin hello",       // 签发人
+			NotBefore: time.Now().Unix(), // 生效时间
+			Subject:   "login",           // 主题
+		}
+		var jwtSecret = []byte(config.Secret)
+		tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		token, err := tokenClaims.SignedString(jwtSecret)
+		if err == nil {
+			result.Message = "登录成功"
+			result.Data = "bearer " + token
+			result.Code = http.StatusOK
+			context.JSON(http.StatusOK, gin.H{
+				"result": result,
+			})
+		} else {
+			result.Message = "登录失败"
+			result.Code = http.StatusOK
+			context.JSON(result.Code, gin.H{
+				"result": result,
+			})
+		}
+	} else {
+		result.Message = "登录失败"
+		result.Code = http.StatusOK
+		context.JSON(result.Code, gin.H{
+			"result": result,
+		})
+	}
 }
--- packages/gin_series/12_jwt/model/result.go
+++ packages/gin_series/12_jwt/model/result.go
@@ -3,5 +3,5 @@ package model
 type Result struct {
 	Code int `json:"code" example:"000"`
 	Message string `json:"message" example:"请求信息"`
-	Date interface{} `json:"data"`
+	Data interface{} `json:"data"`
 }
